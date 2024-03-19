cde65686ff2c72666c84a17248ebdc3ab4a2a230
roqkfwk87 <tuicswrjj86874@naver.com>
2024-03-19 17:06:44 +0900

    springboot

--- springworkspace/basic/src/main/java/com/roqkfwk/basic/controller/BasicController.java
+++ springworkspace/basic/src/main/java/com/roqkfwk/basic/controller/BasicController.java
@@ -1,5 +1,93 @@
 package com.roqkfwk.basic.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+
+
+
+// Controller 레이어 :
+// - 클라이언트와 서버간의 접점 
+// - 클라이언트의 요청을 받고 해당 요청에 대한 응답을 클라이언트께 돌려주는 영역
+// - 각 요청에 해당하는 URL 메서드를 작성하는 영역 
+
+//@RestController : JSON 형태의 Response Body를 반환하는 Controller임을 명시
+//@RestController = @Controller + @ResponseBody
+@RestController
+// @RequestMapping() : HTTP 요청에 클래스와 메서드를 매핑하기 위한 어노테이션 
+// @RequestMapping(value="/main", method={RequestMethod.GET})
+// HTTP GET loalhost:4000/main/**
+@RequestMapping("/main")
 public class BasicController {
+
+    // HTTP GET localhost:4000/main/
+    @RequestMapping(value = "/", method = {RequestMethod.GET})
+    public String getHello () {
+        return "Hello Springboot!!";
+    }
+
+    // HTTP GET Method : 클라이언트가 서버로부터 데이터를 받기를 원한다면 사용하는 메서드
+    // postman에 get으로 설정하고 localhost:4000/main/apple 적음
+    //@GetMapping() : RequestMapping 기능을 GET Mathod에 한정시킨 것 (가독성 + 안정성)
+    @GetMapping("/apple")
+    public String getApple() {
+        return "Get Mapping으로 만든 메서드";
+    }
     
+    // HTTP POST Method : 클라이언트가 서버에 데이터를 작성하기 원할때 사용하는 메서드
+    // postman에 post으로 설정하고 localhost:4000/main/apple 적음
+    // PostMapping() : RequestMapping 기능을 Post Method 한정시킨 것 (가독성 + 안정성)
+    @PostMapping("/apple")
+    public String postApple() {
+        return "Post Mapping으로 만든 메서드";
+    }
+    
+    // HTTP PUT Method : 클라이언트가 서버에 있는 리소스 전체를 수정하고 싶을 때 사용하는 메서드
+    // @PutMapping() : RequestMapping 기능을 Put Method에 한정시킨 것 (가독성 + 안전성)
+    @PutMapping("/apple")
+    public String putApple() {
+        return "Put Mapping으로 만든 메서드";
+    }
+
+    // HTTP PATCH Method : 클라이언트가 서버에 있는 리소스 일부를 수정하고 싶을때 사용하는 메서드 
+    // @PatchMapping() : RequestMapping 기능을 Patch Method에 한정시킨 것 (가독성 + 안정성)
+    @PatchMapping("/apple")
+    public String patchApple () {
+        return "Patch Mapping으로 만든 메서드";
+    }
+
+    // HTTP DELETE Method : 클라이언트가 서버에 있는 리소스를 삭제하고 싶을 때 사용하는 메서드 
+    // @DeleteMapping() : RequestMapping 기능을 Delete Method에 한정시킨 것 (가독성 + 안정성)
+    @DeleteMapping("/apple")
+    public String deleteApple () {
+        return "Delete Mapping으로 만든 메서드";
+    }
+
+    // Mathod + URL Pattern이 중복되면 런타임 중에 에러가 발생 
+    // @DeleteMapping("/apple")
+    // public String deleteApple () {
+    //     return "Delete Mapping으로 만든 메서드";
+    // }
 }
+
+// 회원가입- post
+// 유저정보 보기- get 방식  
+// 게시물 수정하기 - put or patch
+// 회원탈퇴 - delete
+// 로그인 - get 방식?
+
+// 게시물 좋아요 - get
+// -> 만약 해당 유저가 특정 피드에 좋아요 이미 되어 있다면 해당 좋아요 레코드를 삭제
+// -> 만약 해당 유저가 특정 피드에 좋아요 되어 있지 않다면 해당 좋아요 레코드를 삽입 
+
+// 사원정보 입력 
+// -> 전체 사원 리스트
