51dbda324437a9ebbe5797c79996c37cad5cd976
roqkfwk87 <tuicswrjj86874@naver.com>
2024-03-25 17:34:40 +0900

    ~2024.03.25

--- springworkspace/basic/src/main/java/com/roqkfwk/basic/controller/BasicController.java
+++ springworkspace/basic/src/main/java/com/roqkfwk/basic/controller/BasicController.java
@@ -3,6 +3,14 @@ package com.roqkfwk.basic.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.roqkfwk.basic.service.BasicService;
+import com.roqkfwk.basic.service.implement.BasicServiceImplement;
+
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -27,12 +35,36 @@ import org.springframework.web.bind.annotation.PathVariable;
 // @RequestMapping(value="/main", method={RequestMethod.GET})
 // HTTP GET loalhost:4000/main/**
 @RequestMapping("/main")
+// final로 선언된 필드 변수에 대해서 생성자를 자동으로 작성함 
+@RequiredArgsConstructor
 public class BasicController {
+
+    // private BasicService service;
+    
+    // 의존성 역전 (DI) : 
+    // - 해당 클래스에 필요한 의존성을 외부(클래스의 인스턴스를 생성하는 위치)에서 주입
+    // - 생성자를 통한 의존성 주입, setter 메서드를 통한 의존성 주입, 필드를 통한 의존성 주입 
+    
+    // 생성자를 통한 의존성 주입을 spring에서 권장 (의존성이 주입되지 않은 경우가 존재할 수 없기 때문)
+    // - spring framework에서 의존성 주입은 제어의 역전 (IoC)를 통해서 spring framework가 진행 (@Component를 사용해서 Spring Bean으로 등록된 Class에 한해서)
+    
+    // Autowired : 등록된 Spring Bean을 제어의 역전을 통해서 의존성을 주입하는 방법
+    // - 단, 생성자를 통한 의존성 주입에는 @Autowired를 생략해도 됨   
+    
+    // @Autowired
+    // public BasicController(BasicService service) {
+        //     this.service = service;
+        // }
+        
+    //! 개발자 질문으로 사용 가능성 있음
+    // 의존성 주입시 클래스로 직접 참조변수를 만들지 않고 인터페이스로 간접적으로 만드는 이유:
+    // 고수준의 모듈에서 저수준의 모듈을 직접 참조하지 않고 추상화를 통해 간접 참조함으로써 각 모듈간의 결합도를 낮춤 -> 코드의 재사용성 향상, 유지보수성 향상
+    private final BasicService service;
 
     // HTTP GET localhost:4000/main/
     @RequestMapping(value = "/", method = {RequestMethod.GET})
     public String getHello () {
-        return "Hello Springboot!!";
+        return service.getHello();
     }
 
     // HTTP GET Method : 클라이언트가 서버로부터 데이터를 받기를 원한다면 사용하는 메서드
@@ -40,7 +72,7 @@ public class BasicController {
     //@GetMapping() : RequestMapping 기능을 GET Mathod에 한정시킨 것 (가독성 + 안정성)
     @GetMapping("/apple")
     public String getApple() {
-        return "Get Mapping으로 만든 메서드";
+        return service.getApple();
     }
     
     // HTTP POST Method : 클라이언트가 서버에 데이터를 작성하기 원할때 사용하는 메서드
