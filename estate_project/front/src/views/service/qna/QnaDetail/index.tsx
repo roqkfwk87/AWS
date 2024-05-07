eec74e4355d85b6e323836958d57a28e0d25808c
roqkfwk87 <tuicswrjj86874@naver.com>
2024-05-07 17:46:01 +0900

    2024.05.07

--- estate_project/front/src/views/service/qna/QnaDetail/index.tsx
+++ estate_project/front/src/views/service/qna/QnaDetail/index.tsx
@@ -1,17 +1,20 @@
-import React, { useEffect, useState } from 'react';
+import React, { ChangeEvent, useEffect, useRef, useState } from 'react';
 import './style.css'
 import { useUserStore } from 'src/stores';
-import { getBoardRequest, increaseViewCountRequest } from 'src/apis/board';
+import { getBoardRequest, increaseViewCountRequest, postCommentRequest } from 'src/apis/board';
 import { useCookies } from 'react-cookie';
 import { useNavigate, useParams } from 'react-router';
 import ResponseDto from 'src/apis/response.dto';
 import { AUTH_ABSOLUTE_PATH, QNA_LIST_ABSOLUTE_PATH } from 'src/constant';
 import { GetBoardResponseDto } from 'src/apis/board/dto/response';
+import { PostCommentRequestDto } from 'src/apis/board/dto/request';
 
 //                    component                    //
 export default function QnaDetail() {
 
     //                    state                    //
+    const commentRef = useRef<HTMLTextAreaElement | null>(null);
+
     const { loginUserId, loginUserRole } = useUserStore();
     const { receptionNumber } = useParams();
 
@@ -77,7 +80,48 @@ export default function QnaDetail() {
         setStatus(status);
         setComment(comment);
     };
+
+    const postCommentResponse = (result: ResponseDto | null) => {
+        
+        const message = 
+            !result ? '서버에 문제가 있습니다.' :
+            result.code === 'AF' ? '권한이 없습니다.' :
+            result.code === 'VF' ? '입력 데이터가 올바르지 않습니다.' : 
+            result.code === 'NB' ? '존재하지 않는 게시물입니다.' :
+            result.code === 'WC' ? '이미 답글이 작성된 게시물입니다.' :
+            result.code === 'DBE' ? '서버에 문제가 있습니다.' : '';
     
+        if (!result || result.code !== 'SU') {
+            alert(message);
+            return;
+        }
+
+        if (!receptionNumber || !cookies.accessToken) return;
+        getBoardRequest(receptionNumber, cookies.accessToken).then(getBoardResponse);
+
+    };
+
+    
+    //                    event handler                    //
+    const onCommentChangeHandler = (event: ChangeEvent<HTMLTextAreaElement>) => {
+        if (status || loginUserRole !== 'ROLE_ADMIN') return;
+        const comment = event.target.value;
+        setComment(comment);
+    
+        if (!commentRef.current) return;
+        commentRef.current.style.height = 'auto';
+        commentRef.current.style.height = `${commentRef.current.scrollHeight}px`;
+        };
+    
+        const onCommentSubmitClickHandler = () => {
+        if (!comment || !comment.trim()) return;
+        if (!receptionNumber || loginUserRole !== 'ROLE_ADMIN' || !cookies.accessToken) return;
+        
+        const requestBody: PostCommentRequestDto = { comment };
+        postCommentRequest(receptionNumber, requestBody, cookies.accessToken).then(postCommentResponse);
+        };
+    
+
     //                    effect                    //
     useEffect(() => {
         if (!cookies.accessToken || !receptionNumber) return;
@@ -105,9 +149,9 @@ export default function QnaDetail() {
             {loginUserRole === 'ROLE_ADMIN' && !status && 
             <div className='qna-detail-comment-write-box'>
                 <div className='qna-detail-comment-textarea-box'>
-                    <textarea className='qna-detail-comment-textarea' placeholder='답글을 작성해주세요.' />
+                    <textarea ref={commentRef} className='qna-detail-comment-textarea' placeholder='답글을 작성해주세요.' value={comment == null ? '' : comment} onChange={onCommentChangeHandler}/>
                 </div>
-                <div className='primary-button'>답글달기</div>
+                <div className='primary-button' onClick={onCommentSubmitClickHandler}>답글달기</div>
             </div>
             }
             {status &&
