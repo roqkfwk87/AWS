dfdaa1346516bb3870d22d787bf5512f7fb1d5a5
roqkfwk87 <tuicswrjj86874@naver.com>
2024-05-09 15:04:24 +0900

    2024.05.09

--- estate_project/front/src/views/service/Local/index.tsx
+++ estate_project/front/src/views/service/Local/index.tsx
@@ -1,16 +1,104 @@
-import React from 'react';
+
+import React, { useState } from 'react';
 import './style.css'
 import SelectBox from 'src/components/SelectBox';
+import { CategoryScale, Chart as ChartJS, LineElement, LinearScale, PointElement, Tooltip } from 'chart.js';
+import { Line } from 'react-chartjs-2';
+
+ChartJS.register(
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Tooltip
+);
 
 //                    component                    //
 export default function Local() {
+
+    const saleOptions = {
+        responsive: false,
+        scales: {
+            y: {
+                min: 150,
+                max: 300,
+                ticks: {
+                    stepSize: 30
+                }
+            }
+        }
+    };
+
+    const leaseOptions = {
+        responsive: false,
+        scales: {
+            y: {
+                min: 150,
+                max: 300,
+                ticks: {
+                    stepSize: 30
+                }
+            }
+        }
+    };
+
+    const monthRentOptions = {
+        responsive: false,
+        scales: {
+            y: {
+                min: 150,
+                max: 300,
+                ticks: {
+                    stepSize: 30
+                }
+            }
+        }
+    };
+
+    //                    state                    //
+    const [selectLocal, setSelectLocal] = useState<string>('');
+
+    //                    event handler                    //
+    const onLocalChangeHandler = (selectLocal: string) => {
+        setSelectLocal(selectLocal);
+    };
+
+    const saleData = {
+        labels: ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12'],
+        datasets: [{
+            label: '매매 평균',
+            data: [225, 224, 224, 224, 200, 211, 225, 224, 224, 224, 200, 211],
+            borderColor: 'rgba(58, 87, 232, 1)',
+            backgroundColor: 'rgba(58, 87, 232, 1)'
+        }]
+    };
+
+    const leaseData = {
+        labels: ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12'],
+        datasets: [{
+            label: '전세 평균',
+            data: [225, 224, 224, 224, 200, 211, 225, 224, 224, 224, 200, 211],
+            borderColor: 'rgba(58, 87, 232, 1)',
+            backgroundColor: 'rgba(58, 87, 232, 1)'
+        }]
+    };
+
+    const monthRentData = {
+        labels: ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12'],
+        datasets: [{
+            label: '월세 평균',
+            data: [225, 224, 224, 224, 200, 211, 225, 224, 224, 224, 200, 211],
+            borderColor: 'rgba(58, 87, 232, 1)',
+            backgroundColor: 'rgba(58, 87, 232, 1)'
+        }]
+    };
 
     //                    render                    //
     return (
         <div id='local-wrapper'>
             <div className='local-top'>
                 <div className='local-search-box'>
-                    <SelectBox />
+                    <SelectBox value={selectLocal} onChange={onLocalChangeHandler} />
                     <div className='primary-button'>검색</div>
                 </div>
                 <div className='local-origin-text'>데이터 출처: KOSIS</div>
@@ -21,7 +109,7 @@ export default function Local() {
                     <div className='local-card-unit'>(단위: 백만원)</div>
                 </div>
                 <div className='local-card-chart-box'>
-                    
+                    <Line width={'1086px'} height={'238px'} options={saleOptions} data={saleData} />
                 </div>
             </div>
             <div className='local-card'>
@@ -30,7 +118,7 @@ export default function Local() {
                     <div className='local-card-unit'>(단위: 백만원)</div>
                 </div>
                 <div className='local-card-chart-box'>
-                    
+                    <Line width={'1086px'} height={'238px'} options={leaseOptions} data={leaseData} />
                 </div>
             </div><div className='local-card'>
                 <div className='local-card-title-box'>
@@ -38,7 +126,7 @@ export default function Local() {
                     <div className='local-card-unit'>(단위: 백만원)</div>
                 </div>
                 <div className='local-card-chart-box'>
-                    
+                    <Line width={'1086px'} height={'238px'} options={monthRentOptions} data={monthRentData} />
                 </div>
             </div>
         </div>
--- estate_project/front/src/views/service/Ratio/index.tsx
+++ estate_project/front/src/views/service/Ratio/index.tsx
@@ -1,7 +1,135 @@
-import React from 'react'
-import './style.css'
+import React, { useState } from 'react';
+import './style.css';
+import SelectBox from 'src/components/Selectbox';
+import { BarElement, CategoryScale, Chart as ChartJS, Legend, LineElement, LinearScale, PointElement, Tooltip } from 'chart.js';
+import { Bar, Line } from 'react-chartjs-2';
 
+ChartJS.register(
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    BarElement,
+    Tooltip,
+    Legend
+);
+
+//                    component                    //
 export default function Ratio() {
-  return <div>Ratio</div>;
 
+    const returnOptions = {
+        responsive: false,
+        scales: {
+            y: {
+                min: 150,
+                max: 300,
+                ticks: {
+                    stepSize: 30
+                }
+            }
+        }
+    };
+
+    const leaseRatioOptions = {
+        responsive: false,
+        scales: {
+            y: {
+                min: 150,
+                max: 300,
+                ticks: {
+                    stepSize: 30
+                }
+            }
+        }
+    };
+
+    const monthRentRatioOptions = {
+        responsive: false,
+        scales: {
+            y: {
+                min: 150,
+                max: 300,
+                ticks: {
+                    stepSize: 30
+                }
+            }
+        }
+    };
+    
+    //                    state                    //
+    const [selectLocal, setSelectLocal] = useState<string>('');
+
+    //                    event handler                    //
+    const onLocalChangeHandler = (selectLocal: string) => {
+        setSelectLocal(selectLocal);
+    };
+
+    const returnData = {
+        labels: ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12'],
+        datasets: [{
+            label: '매매 평균',
+            data: [225, 224, 224, 224, 200, 211, 225, 224, 224, 224, 200, 211],
+            borderColor: 'rgba(58, 87, 232, 1)',
+            backgroundColor: 'rgba(58, 87, 232, 1)'
+        }]
+    };
+
+    const leaseRatioData = {
+        labels: ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12'],
+        datasets: [{
+            label: '전세 평균',
+            data: [225, 224, 224, 224, 200, 211, 225, 224, 224, 224, 200, 211],
+            borderColor: 'rgba(58, 87, 232, 1)',
+            backgroundColor: 'rgba(58, 87, 232, 1)'
+        }]
+    };
+
+    const monthRentRatioData = {
+        labels: ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12'],
+        datasets: [{
+            label: '월세 평균',
+            data: [225, 224, 224, 224, 200, 211, 225, 224, 224, 224, 200, 211],
+            borderColor: 'rgba(58, 87, 232, 1)',
+            backgroundColor: 'rgba(58, 87, 232, 1)'
+        }]
+    };
+
+    //                    render                    //
+    return (
+        <div id='local-wrapper'>
+            <div className='local-top'>
+                <div className='local-search-box'>
+                    <SelectBox value={selectLocal} onChange={onLocalChangeHandler} />
+                    <div className='primary-button'>검색</div>
+                </div>
+                <div className='local-origin-text'>데이터 출처: KOSIS</div>
+            </div>
+            <div className='local-card'>
+                <div className='local-card-title-box'>
+                    <div className='local-card-title'>수익률 평균</div>
+                    <div className='local-card-unit'>(단위: 백만원)</div>
+                </div>
+                <div className='local-card-chart-box'>
+                    <Line width={'1086px'} height={'238px'} options={returnOptions} data={returnData} />
+                </div>
+            </div>
+            <div className='local-card'>
+                <div className='local-card-title-box'>
+                    <div className='local-card-title'>매매가격 대비 전세 비율</div>
+                    <div className='local-card-unit'>(단위: 백만원)</div>
+                </div>
+                <div className='local-card-chart-box'>
+                    <Bar width={'1086px'} height={'238px'} options={leaseRatioOptions} data={leaseRatioData} />
+                </div>
+            </div><div className='local-card'>
+                <div className='local-card-title-box'>
+                    <div className='local-card-title'>전세 가격 대비 월세 보증금 비율</div>
+                    <div className='local-card-unit'>(단위: 백만원)</div>
+                </div>
+                <div className='local-card-chart-box'>
+                    <Line width={'1086px'} height={'238px'} options={monthRentRatioOptions} data={monthRentRatioData} />
+                </div>
+            </div>
+        </div>
+    )
 }
